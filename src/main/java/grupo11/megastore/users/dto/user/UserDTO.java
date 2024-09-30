f31e0138bb2c0c6963ff58cc4327c2813df26637
lucasleone03 <lucasagustinleone2003@gmail.com>
2024-09-30 09:35:06 -0300

    Change camelCase to snake_case in userDTO.

--- src/main/java/grupo11/megastore/users/dto/user/UserDTO.java
+++ src/main/java/grupo11/megastore/users/dto/user/UserDTO.java
@@ -1,13 +1,17 @@
 package grupo11.megastore.users.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
-
 import grupo11.megastore.users.model.Role;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserDTO {
     
     private Long id;
