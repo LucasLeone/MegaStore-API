ffc294b9c2108e9ce32a2e89703b98695189ef23
lucasleone03 <lucasagustinleone2003@gmail.com>
2024-12-06 09:43:04 -0300

    Add test settings.

--- src/test/java/grupo11/megastore/MegastoreApplicationTests.java
+++ src/test/java/grupo11/megastore/MegastoreApplicationTests.java
@@ -2,8 +2,10 @@ package grupo11.megastore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MegastoreApplicationTests {
 
 	@Test
---
+++ src/test/java/grupo11/megastore/config/TestConfig.java
@@ -0,0 +1,34 @@
+package grupo11.megastore.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@TestConfiguration
+@Profile("test")
+public class TestConfig {
+    
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+
+    @Bean
+    public SecurityFilterChain tSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
